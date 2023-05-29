ab8be8edc155f2a5fda2138ea5f425262a705344
Anna Spiridonova <spiridonova.aniutka@yandex.ru>
2023-05-29 17:47:46 +0300

    feat: add logger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -13,7 +14,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(requestLogger);
 app.use(router);
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
